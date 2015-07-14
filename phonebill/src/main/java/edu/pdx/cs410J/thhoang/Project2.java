3121e41a337310eea5bf8bc24aef0f4f8ba1ec20
Thanh Hoang <thhoang@pdx.edu>
2015-07-13 18:54:25 -0700

    Project 2 - first version

--- phonebill/src/main/java/edu/pdx/cs410J/thhoang/Project2.java
+++ phonebill/src/main/java/edu/pdx/cs410J/thhoang/Project2.java
@@ -1,5 +1,11 @@
 package edu.pdx.cs410J.thhoang;
 
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  * The main class for CS410J Phone Bill Project 2,
  * (working with text file)
@@ -7,8 +13,170 @@ package edu.pdx.cs410J.thhoang;
 public class Project2 {
 
     public static void main(String[] args) {
+        String customer = null;
+        String callerNumber = null;
+        String calleeNumber = null;
+        String startDate = null;
+        String startTime = null;
+        String endDate = null;
+        String endTime = null;
+        ArrayList<String> options = new ArrayList<String>();
+        ArrayList<String> arguments = new ArrayList<String>();
+        File file = null;
+
+        if (args.length == 0) {
+            printErrMessageAndExit("Missing command line arguments");
+        }
+
+        for (String arg : args) {
+            if (arg.charAt(0) == '-')
+                options.add(arg);
+            else
+                arguments.add(arg);
+        }
+
+        for (String opt : options) {
+            if (opt.equals("-README")) {
+                printReadMeAndExit();
+            }
+        }
+
+        // check weird option
+        for (String opt: options) {
+
+            if (!opt.equals("-print") || opt.equals("-README") || (!opt.equals("-textFile")))
+                printErrMessageAndExit("Error: option " + opt + " not found");
+        }
+
+
+
+
+        if (arguments.size() > 7) {
+            printErrMessageAndExit("Error: There are extraneous command line arguments ");
+        }
+
+        for (String arg : arguments) {
+
+            if (customer == null) {
+                customer = arg;
+            } else if (callerNumber == null) {
+                callerNumber = arg;
+            } else if (calleeNumber == null) {
+                calleeNumber = arg;
+            } else if (startDate == null) {
+                startDate = arg;
+            } else if (startTime == null) {
+                startTime = arg;
+            } else if (endDate == null) {
+                endDate = arg;
+            } else if (endTime == null) {
+                endTime = arg;
+            }
+        }
+
+        // print err message and exit if one of argument is missing
+        if (customer == null) {
+            printErrMessageAndExit("Missing customer");
+        } else if (callerNumber == null) {
+            printErrMessageAndExit("Missing callerNumber");
+        } else if (calleeNumber == null) {
+            printErrMessageAndExit("Missing calleeNumber");
+        } else if (startDate == null) {
+            printErrMessageAndExit("Missing startDate");
+        } else if (startTime == null) {
+            printErrMessageAndExit("Missing startTime");
+        } else if (endDate == null) {
+            printErrMessageAndExit("Missing endDate");
+        } else if (endTime == null) {
+            printErrMessageAndExit("Missing endTime");
+        }
+
+        // validate arguments format
+        if (!checkPhoneNumber(callerNumber)) {
+            printErrMessageAndExit("callerNumber is not input in the right format: nnn-nnn-nnnn (where n is 0-9)");
+        } else if (!checkPhoneNumber(calleeNumber)) {
+            printErrMessageAndExit("calleeNumber is not input in the right format: nnn-nnn-nnnn (where n is 0-9)");
+        } else if (!checkDateFormat(startDate)) {
+            printErrMessageAndExit("startDate is not input in the right format: mm/dd/yyyy");
+        } else if (!checkTimeFormat(startTime)) {
+            printErrMessageAndExit("startTime is not input in the right format: hh:mm");
+        } else if (!checkDateFormat(endDate)) {
+            printErrMessageAndExit("endDate is not input in the right format: mm/dd/yyyy");
+        } else if (!checkTimeFormat(endTime)) {
+            printErrMessageAndExit("endTime is not input in the right format: hh:mm");
+        }
+
+        // add new phone call to collection
+        AbstractPhoneBill newBill;
+        AbstractPhoneCall aCall;
+
+        try {
+            String start = startDate + " " + startTime;
+            String end = endDate + " " + endTime;
+
+            newBill = new PhoneBill(customer);
+            aCall = new PhoneCall(callerNumber, calleeNumber, start, end);
+            newBill.addPhoneCall(aCall);
+
+            System.out.println("Successful added new phone call");
+
+            for (String s : options) {
+                if (s.equals("-print")) {
+                    System.out.println("Description of new phone call: ");
+                    System.out.println(aCall.toString());
+                }
+            }
+
+        } catch (Exception e) {
+            printErrMessageAndExit("Fail to add new phone call");
+        }
+
 
         System.exit(1);
+    }
+
+    /**
+     * Validate a date is in mm/dd/yyyy or not
+     * @param date string date format in form mm/dd/yyyy
+     * @return boolean
+     */
+    private static boolean checkDateFormat(String date) {
+
+        if (date.matches("\\d{2}/\\d{2}/\\d{4}"))
+            return true;
+        else if (date.matches("\\d{1}/\\d{2}/\\d{4}"))
+            return true;
+        else if (date.matches("\\d{2}/\\d{1}/\\d{4}"))
+            return true;
+        else return date.matches("\\d{1}/\\d{1}/\\d{4}");
+
+    }
+
+    /**
+     * Validate if time is in hh:mm format or not
+     * @param time time string in form hh:mm
+     * @return boolean
+     */
+    private static boolean checkTimeFormat(String time) {
+
+        if (time.matches("\\d{2}:\\d{2}"))
+            return true;
+        else if (time.matches("\\d{2}:\\d{1}")) {
+            return true;
+        } else if (time.matches("\\d{1}:\\d{2}")) {
+            return true;
+        } else return time.matches("\\d{1}:\\d{1}");
+    }
+
+    /**
+     * Validate if the phone number is in the format nnn-nnn-nnnn
+     * @param phoneNumberString phone number string in form nnn-nnn-nnnn
+     * @return boolean
+     */
+    private static boolean checkPhoneNumber(String phoneNumberString) {
+
+        return phoneNumberString.matches("\\d{3}-\\d{3}-\\d{4}");
+
     }
 
     /**
@@ -16,55 +184,36 @@ public class Project2 {
      */
     private static void printReadMeAndExit() {
 
-        System.out.println("Project 1");
+        System.out.println("Project 2");
         System.out.println("@author: Thanh Hoang");
-        System.out.println("This project is designed to build fundamental PhoneBill and PhoneCall classes, and project1 class that parses the commmand line.");
+        System.out.println("This project is built from project 1, and it can handle read and write PhoneBill from or to text file.");
+
         System.out.println("The command line has to be in this order:");
         System.out.println("\t[options] <args>");
         System.out.println("options are:");
         System.out.println("\t-print\t\tprints a description of the new phone call");
         System.out.println("\t-README\t\tprints a README for this project and exits\n");
+        System.out.println("\t-textFile file \t\t Where to read/write the phone bill");
         System.out.println("args are (in this order):");
         System.out.println("\tcustomer\tperson whose phone bill we're modeling");
         System.out.println("\tcallerNumber\tPhone number of caller (in format nnn-nnn-nnnn)");
         System.out.println("\tcalleeNumber\tphone number of person who was called (in format nnn-nnn-nnnn)");
         System.out.println("\tstartTime\tDate and time call began (24- hour time)");
         System.out.println("\tendTime \tDate and time call end (24-hour time)");
-        System.out.println("\t\t\tDate and time in this formate :\tmm/dd/yyyy hh:mm");
+        System.out.println("\t\t\tDate and time in this format :\tmm/dd/yyyy hh:mm");
 
         System.exit(1);
 
-
-        /*
-        File f = new File("README.txt");
-
-        try {
-            if (!f.exists())
-                f.createNewFile();
-            FileReader fr = new FileReader(f);
-            BufferedReader br = new BufferedReader(fr);
-
-            StringBuffer sb = new StringBuffer();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-
-            System.out.print(sb.toString());
-            System.exit(1);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println(" ** " + e);
-            System.exit(1);
-        }
-        */
     }
 
-
+    /**
+     * Print out error message and exit program.
+     * @param message Error message
+     */
+    private static void printErrMessageAndExit(String message) {
+        System.err.println(message);
+        System.exit(1);
+    }
 
 
 }
--- phonebill/src/test/java/edu/pdx/cs410J/thhoang/Project2Test.java
+++ phonebill/src/test/java/edu/pdx/cs410J/thhoang/Project2Test.java
@@ -1,9 +1,14 @@
 package edu.pdx.cs410J.thhoang;
 
+import edu.pdx.cs410J.AbstractPhoneBill;
 import edu.pdx.cs410J.AbstractPhoneCall;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import org.hamcrest.Matcher;
 import org.junit.Test;
+import org.omg.CORBA.OBJ_ADAPTER;
+
+import java.io.File;
+import java.util.Objects;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
@@ -15,6 +20,52 @@ import static org.junit.Assert.assertTrue;
  * Tests the functionality in project 2
  */
 public class Project2Test extends InvokeMainTestCase {
+
+
+    @Test
+    public void testTextDumper() {
+
+      /*  PhoneCall call1 = new PhoneCall("000-111-2222", "000-222-3333", "1/15/2015 10:10", "1/15/2015 10:30");
+        PhoneCall call2 = new PhoneCall("000-333-2222", "000-333-5555", "1/20/2015 14:22", "1/20/2015 15:99");
+
+        PhoneBill bill = new PhoneBill("A cat");
+        bill.addPhoneCall(call1);
+        bill.addPhoneCall(call2);
+
+        TextDumper td = new TextDumper(new File("abc.txt"));
+        try {
+            td.dump(bill);
+        } catch (Exception e) {
+
+        }
+*/
+
+    }
+
+    @Test
+    public void testTextParser() {
+
+        AbstractPhoneCall aCall = null;
+        AbstractPhoneBill bill = null;
+
+        TextParser tp = new TextParser(new File("abc.txt"));
+
+        try {
+            bill = tp.parse();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        if (bill == null)
+            System.out.println("false");
+ /*       System.out.println(bill.toString());
+
+        for (Object object: bill.getPhoneCalls()){
+            aCall =(AbstractPhoneCall) object;
+            System.out.println(aCall.toString());
+        }
+*/
+    }
 
     private MainMethodResult invokeMain(String... args) {
         return invokeMain(Project2.class, args);
