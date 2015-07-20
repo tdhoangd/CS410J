77aeb673bbe2a4840e622c61dc9b22cf233fa555
Thanh Hoang <thhoang@pdx.edu>
2015-07-20 11:47:06 -0700

    project 3

---
+++ phonebill/src/main/java/edu/pdx/cs410J/thhoang/PrettyPrinter.java
@@ -0,0 +1,18 @@
+package edu.pdx.cs410J.thhoang;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.PhoneBillDumper;
+
+import java.io.IOException;
+
+
+/**
+ * The class that implements <code>PhoneBillDumper</code> interface and creates
+ * a nicely formatted textual presentation of the phone calls in a phone bill
+ */
+public class PrettyPrinter implements PhoneBillDumper {
+    @Override
+    public void dump(AbstractPhoneBill bill) throws IOException {
+
+    }
+}
