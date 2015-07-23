e7fcd2d951907252388aefb5d6cbf130182197eb
Thanh Hoang <thhoang@pdx.edu>
2015-07-22 17:13:04 -0700

    :3

---
+++ koans/Quizzes - CS-410-009_ SELECTED TOPICS TOP_ ADV PROGRAMMING IN JAVA (Summer 2015) - Portland State University_files/feedback_balloon_display.js
@@ -0,0 +1,19 @@
+﻿function showAtomFeedback( event, labelId, contents ) {
+	var label = UI.GetById( labelId );
+	fbBalloon.contents.innerHTML = contents;
+	fbBalloon.Show( event, label );
+}
+function showDropDownFeedbackForSelectList( obj, objName ) {
+	var feedback = UI.GetByName( objName + '_' + obj.GetSelectedValue() + '_id' );
+	if( feedback) {
+		fbBalloon.contents.innerHTML = feedback.value;
+		fbBalloon.Show( null, obj );
+	}
+}
+function showDropDownFeedback( event, obj ) {
+	var feedback = UI.GetByName( obj.name + '_' + obj.GetSelectedValue() + '_id' );
+	if( feedback) {
+		fbBalloon.contents.innerHTML = feedback.value;
+		fbBalloon.Show( event, obj );
+	}
+}
